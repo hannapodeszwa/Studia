0d474c7257e9ecb451eebd7ce5a53006f207064c
hannapodeszwa <hanna.podeszwa@gmail.com>
2018-11-20 23:38:21 +0100

    projekt1

---
+++ projekt/ConsoleApplication1/ConsoleApplication1/struktury.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//#ifndef STRUKTURY_H  
+//#define STRUKTURY_H
+#include <string>
+#include<sstream>
+
+struct trasa
+{
+	std::string miasto1, miasto2;
+
+	double odleglosc;
+};
+
+#endif
