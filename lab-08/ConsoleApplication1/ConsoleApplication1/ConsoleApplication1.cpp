f0297268b09463236a2b23cc2b0c08e32577fd48
hannapodeszwa <hanna.podeszwa@gmail.com>
2018-11-28 15:11:42 +0100

    lista

--- lab-08/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ lab-08/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,6 +10,7 @@
 
 char tablica(std::istream & wejscie, char tab[8][8])
  {
+	 std::ifstream wejscie("plik");
 	std::string szachowinca;
 	while(wejscie >> szachowinca)
 	//while (std::getline(wejscie, szachownica))
@@ -34,18 +35,40 @@ bool funkcja(tab)
 		{
 			int liczba_hetmanow = 0;
 			for (int k = 0; k < M; k++) // dla kazdego pola w wierszu
+			{
 				if (tab[w][k] == 'H')
 					liczba_hetmanow++;
-
+			}
 		}
+		if (liczba_hetmanow >1)
+			return false;
 
 		// sprawdzenie kolumn
-
+		for (int k = 0; k < M; k++)
+		{
+			int liczba=0;
+			for (int w = 0; w < M; w++)
+			{
+				if (tab[w][k] == 'H')
+					liczba++;
+			}
+			if (liczba >1)
+				return false;
+		}
 
 		// sprawdzenie przekatnych
 
-
-
+		for (int w = 0; w < M; w++)  // dla kazdego wiersza
+		{
+			int liczba_hetmanow = 0;
+			for (int k = 0; k < M; k++) // dla kazdego pola w wierszu
+			{
+				if (tab[w][k] == 'H')
+					liczba_hetmanow++;
+			}
+		}
+		if (liczba_hetmanow >= 1)
+			return false;
 
 	/*	for (int i = 0; i < 8;i++)
 		{
